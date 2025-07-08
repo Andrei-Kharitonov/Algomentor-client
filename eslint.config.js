19b4467b9794fb4dec28cbce1766496cb80636bf
Andrei-Kharitonov <89729599+Andrei-Kharitonov@users.noreply.github.com>
2025-07-08 13:43:36 +0400

    feat #9: Настроил eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import css from '@eslint/css';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,ts,jsx,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,5 +20,30 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'semi': ['error', 'always', { 'omitLastInOneLineBlock': true, 'omitLastInOneLineClassBody': true }],
+      'no-extra-semi': 'error',
+      'semi-spacing': ['error', { 'before': false, 'after': true }],
+      'semi-style': ['error', 'last'],
+      'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+      'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 0, 'maxBOF': 0 }],
+      'indent': ['error', 2], // tab size
+      'quotes': ['error', 'single', { 'allowTemplateLiterals': true, 'avoidEscape': true }],
+      'jsx-quotes': ['error', 'prefer-double'],
+      'no-console': 'warn'
+    }
   },
-])
+  {
+    files: ['**/*.{css,scss}'],
+    plugins: {
+      css,
+    },
+    language: 'css/css',
+    rules: {
+      'css/no-duplicate-imports': 'error',
+      'css/no-empty-blocks': 'error',
+      'css/no-invalid-at-rules': 'error',
+      'css/no-invalid-properties': 'error',
+    },
+  },
+]);
